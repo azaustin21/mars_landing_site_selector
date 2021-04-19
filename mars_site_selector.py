fef5e4831b5fd6057192bb3e93ec3f5d11ba7327
Alex Austin <azaustin21@gmail.com>
2021-04-19 18:59:49 -0500

    Creating basic window and call map of Mars

---
+++ mars_site_selector.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import numpy as np
+import cv2 as cv
+
+# CONSTANTS: User Input:
+IMG_GRAY = cv.imread('mola_1024x501.png', cv.IMREAD_GRAYSCALE)
+IMG_COLOR = cv.imread('mola_color_1024x506.png')
+RECT_WIDTH_KM = 670
+RECT_HT_KM = 335
+MAX_ELEV_LIMIT = 55
+NUM_CANDIDATES = 20
+MARS_CIRCUM = 21344
+
+# CONSTANTS: Derived:
+IMG_HT, IMG_WIDTH = IMG_GRAY.shape
+PIXELS_PER_KM = IMG_WIDTH / MARS_CIRCUM
+RECT_WIDTH = int(PIXELS_PER_KM * RECT_WIDTH_KM)
+RECT_HT = int(PIXELS_PER_KM * RECT_HT_KM)
+LAT_30_N = int(IMG_HT / 3)
+LAT_30_S = LAT_30_N * 2
+STEP_X = int(RECT_WIDTH / 2)
+STEP_Y = int(RECT_HT / 2)
+
+screen = tk.Tk()
+canvas = tk.Canvas(screen, width=IMG_WIDTH, height=IMG_HT + 130)
